0953fc1a2606f26fa105d2d0b096badac3e20c87
Pawel Sobaniec <infipablosoban@gmail.com>
2023-03-23 22:02:11 +0100

    make layout

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { MantineProvider } from '@mantine/core';
+
 import Root from './routes/root';
 import ErrorPage from './error-page';
 import Country from './components/Country';
@@ -30,6 +32,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
     <RouterProvider router={router} />
+    </MantineProvider>
   </React.StrictMode>,
 )
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,18 +1,36 @@
 import { Outlet } from "react-router-dom"
 import Footer from "../components/Footer"
-import Header from "../components/Header"
+import HeaderAction from "../components/Header"
 import Sidemenu from "../components/Sidemenu"
 
 function Root() {
   return (
-    <>
-    <Header />
-    <main>
+    <div className="root">
+    <HeaderAction links={
+        [
+            {
+                link: "/",
+                label: "Home",
+                links: []
+            },
+            {
+                link: "/",
+                label: "Home",
+                links: []
+            },
+            {
+                link: "/",
+                label: "Home",
+                links: []
+            }
+        ]
+    }/>
+    <main className="main">
         <Sidemenu />
         <Outlet />
     </main>
     <Footer />
-    </>
+    </div>
   )
 }
 
